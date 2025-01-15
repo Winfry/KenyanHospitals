3140a6145a516b9b2ccbfe873566907d22ab57bb
Winfry <Winfrynyarangi@gmail.com>
2025-01-15 23:07:59 +0300

    KenyanHospitals

--- Reco.py
+++ Reco.py
@@ -240,7 +240,7 @@ if __name__ == "__main__":
 # Hospital recommendation function
 def recommend_hospitals(county, condition="diabetes"):
     # Filter hospitals based on county and condition (if needed)
-    filtered_hospitals = hospitals[hospitals['COUNTY'].str.contains(county, case=False)]
+    filtered_hospitals = data[data['COUNTY'].str.contains(county, case=False)]
     if condition == "diabetes":
         filtered_hospitals = filtered_hospitals[filtered_hospitals['SPECIALTY'] == 'Diabetes Care']
     return filtered_hospitals[['HOSPITAL_NAME', 'ADDRESS']]    
