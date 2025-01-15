415045b9898df896bb17009ccca22ecf277ceb30
Winfry <Winfrynyarangi@gmail.com>
2025-01-15 23:01:56 +0300

    KenyanHospitals

--- Reco.py
+++ Reco.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import numpy as np
 
 # Load the machine learning model for diabetes prediction
 diabetes_model = joblib.load(open('trained_model.sav', 'rb'))
@@ -34,16 +35,4 @@ def recommend_hospitals(county, hospital_type):
 def main():
     st.title("Unified Health App")
 
-# creating a function for prediction
-def diabetes_prediction(input_data):
-    # changing the input_data to numpy array
-    input_data_as_numpy_array = np.asarray(input_data)
-    # reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
-    prediction = loaded_model.predict(input_data_reshaped)
-    print(prediction)
-    if prediction[0] == 0:
-        return 'Not Diabetic'
-    else:
-        return 'Diabetic'
-    
+
